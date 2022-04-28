4ca8b11006e3be24854eacecd61d9336418146e4
ravii-sa <ravinduhapuarachchi@gmail.com>
2022-04-28 08:13:33 +0530

    added sale.php

---
+++ sale.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Submit sale</title>
+</head>
+<body>
+    <?php
+        include("templates/header.php");
+    ?>
+
+    <!body
+        submit sale form
+    >
+
+    <?php
+        include("templates/footer.php");
+    ?>
+</body>
+</html>
