633c50f1bc66519718ba10094f5d4e02f2e0a9f6
it21294198 <99207728+it21294198@users.noreply.github.com>
2022-05-01 13:39:42 +0530

    first header and footer

--- templates/header.php
+++ templates/header.php
@@ -1,3 +1,28 @@
-<!from body tag to navigation bar>
+        <header>
 
-header
+        <div class="head1">
+        <!--Company logo-->
+        <img class="logo" src="logo.png" alt="Golden Lands">
+        </div>
+
+        <!--Main Menu or Navigation Bar-->
+        <div class="toolbar">
+        <ul>
+            <li class="">Home</li>
+            <li class="">Land Request</li>
+            <li class="">Sell Your Land</li>
+            <li class="">Submit a Request</li>
+        </ul>
+        
+        <ul class="logins">
+            <li>Register</li>
+            <li>Login</li>
+             <!--User's image-->
+            <li><img class="profilepic" src="profile.png" alt="UserImg"></li>
+        </ul>
+        </div>
+
+        <div class="navigation">
+            Home >Register >
+        </div>
+        </header>
