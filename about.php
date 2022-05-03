0c7106d837f60a21790a99ff44a9fed5b41b64fe
it21294198 <99207728+it21294198@users.noreply.github.com>
2022-05-03 13:12:34 +0530

    Merge branch 'main' of https://github.com/ravii-sa/landsale

--- about.php
+++ about.php
@@ -1,1 +1,24 @@
-<!--about>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Home</title>
+</head>
+<body>
+    <?php
+        include("php/templates/header.php");
+    ?>
+
+    <!--body-->
+    <h1>About Us</h1>
+
+    <p>pppppp</p>
+
+    <h1>Terms and Conditions<h1>
+    
+    <p>ppppp</p>
+
+    <?php
+        include("php/templates/footer.php");
+    ?>
+</body>
+</html>
