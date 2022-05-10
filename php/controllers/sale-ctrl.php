b097eda52a5dae6dd2cb67cac12734aec00c0329
ravii-sa <ravinduhapuarachchi@gmail.com>
2022-05-10 22:13:33 +0530

    with old controllers

---
+++ php/controllers/sale-ctrl.php
@@ -0,0 +1,21 @@
+<?php 
+    require_once("php/includes/dbFunctions.php");
+    require_once("php/includes/utilFunctions.php");
+
+    $values = array(); //to store post details
+
+    if (isset($_GET['id'])) //check if page is accessed with correct parameters
+    {
+        $id = $_GET['id'];  //get post id
+        if (is_numeric($id)) //validate id
+        {
+            $values = getSale((int)$id); //get details from the database
+        }
+    }
+    else
+    {
+        //todo error page
+        echo "could not find that post";
+        die();
+    }
+?>
