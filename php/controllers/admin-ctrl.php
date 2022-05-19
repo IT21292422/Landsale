17c832aaab39fff7c7e8881eaf5c106ee47fe3bc
it21294198 <99207728+it21294198@users.noreply.github.com>
2022-05-19 15:08:22 +0530

    addtablestyle

--- php/controllers/admin-ctrl.php
+++ php/controllers/admin-ctrl.php
@@ -1,8 +1,29 @@
 <?php
 
+$username=$_POST['user_id'];
+$password=$_POST['first_name'];
 
-//if(isset($_POST['Create'])){
-    echo "hello";
-//}
+// building the configaration settings
+$con= new mysqli("localhost","root","","register");
+
+$result1=$con->connect_error;
+
+if($result1){
+    die("failed".$result1);
+}
+
+echo "connected!!!!!!!!!!";
+
+$sql="INSERT INTO user VALUES ('$username', '$password')";
+
+$result2=$con->query($sql);
+
+if($result2){
+    echo "added!!!!!!!!";
+}else {
+    echo "failed!!!!!!!!!".$con->error;
+}
+
+$con->close();
 
 ?>
