717a942c1d2642e5f41d4eddfe669a9f8e7c6085
it21294198 <99207728+it21294198@users.noreply.github.com>
2022-05-19 15:56:14 +0530

    alt

--- php/controllers/admin-ctrl.php
+++ php/controllers/admin-ctrl.php
@@ -1,10 +1,10 @@
 <?php
 
-$username=$_POST['user_id'];
-$password=$_POST['first_name'];
+$user_id=$_POST['user_id'];
+$first_name=$_POST['first_name'];
 
 // building the configaration settings
-$con= new mysqli("localhost","root","","register");
+//$con= new mysqli("localhost","root","","landsale");
 
 $result1=$con->connect_error;
 
@@ -14,9 +14,9 @@ if($result1){
 
 echo "connected!!!!!!!!!!";
 
-$sql="INSERT INTO user VALUES ('$username', '$password')";
+$sql2="INSERT INTO complaint VALUES ('$user_id', '$first_name','$la')";
 
-$result2=$con->query($sql);
+$result2=$con->query($sql2);
 
 if($result2){
     echo "added!!!!!!!!";
