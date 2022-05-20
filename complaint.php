610ec3db8e9de1bf889d88ba4cfdc6299d4892dd
it21294198 <99207728+it21294198@users.noreply.github.com>
2022-05-20 13:37:02 +0530

    addtab1

--- complaint.php
+++ complaint.php
@@ -47,8 +47,52 @@
 
 include_once 'php\includes\dbcon.php';
 
-//Request table
+//Sale table
 $sql1="SELECT complaint_id,description,reviewed,complaint_type,sale_id,user_id,create_date FROM sale_complaints";
+
+$result2=$con->query($sql1);
+
+//Shows the Request table details
+if ($result2->num_rows > 0) {
+
+    echo("<h1>Sale Complaints</h1>");
+    
+    echo("<table class='table1'>");
+
+    echo("<th>Complaint ID</th>");
+    echo("<th>Complaint Type</th>");
+    echo("<th>Sale ID</th>");
+   // echo("<th>Created date</th>");
+    echo("<th>User ID</th>");
+    echo("<th>Description</th>");
+    echo("<th>Reviewed</th>");
+    echo("<th>Action</th>");
+
+    while($row = $result2->fetch_assoc()) {
+        echo "<tr>";
+        echo "<td>".$row["complaint_id"]."</td>";
+        echo "<td>".$row["complaint_type"]."</td>";
+        echo "<td>".$row["sale_id"]."</td>";
+       // echo "<td>".$row["create date"]."</td>";
+        echo "<td>".$row["user_id"]."</td>";
+        echo "<td>".$row["description"]."</td>";
+        echo "<td>".$row["reviewed"]."</td>";
+        echo "<th>delete</th>";
+        echo "</tr>";
+    }
+    echo ("</table>");
+
+    }else{
+        echo ('<h1 class="warnings">The Sale complaint table is empty</h1>');
+    }
+
+$con->close();
+
+/////////////////////////////////////////////////////////////////////////////
+include_once 'php\includes\dbcon.php';
+
+//Request table
+$sql1="SELECT complaint_id,description,reviewed,complaint_type,sale_id,user_id,create_date FROM request_complaints ";
 
 $result2=$con->query($sql1);
 
@@ -60,63 +104,24 @@ if ($result2->num_rows > 0) {
     echo("<table class='table1'>");
 
     echo("<th>Complaint ID</th>");
-    echo("<th>Reviewed</th>");
-    echo("<th>Created date</th>");
     echo("<th>Complaint Type</th>");
-    echo("<th></th>");
+    echo("<th>Sale ID</th>");
+   // echo("<th>Created date</th>");
+    echo("<th>User ID</th>");
     echo("<th>Description</th>");
+    echo("<th>Reviewed</th>");
     echo("<th>Action</th>");
 
     while($row = $result2->fetch_assoc()) {
         echo "<tr>";
         echo "<td>".$row["complaint_id"]."</td>";
+        echo "<td>".$row["complaint_type"]."</td>";
+        echo "<td>".$row["sale_id"]."</td>";
+       // echo "<td>".$row["create date"]."</td>";
         echo "<td>".$row["user_id"]."</td>";
-        echo "<td>".$row["date"]."</td>";
-        echo "<td>".$row["type"]."</td>";
-        echo "<td>".$row["review"]."</td>";
         echo "<td>".$row["description"]."</td>";
-        ?>
-        <td><a class="delete1" href="php\controllers\complaint-ctrl.php?id=<?php echo $row["id"]; ?>">Delete</a></td>
-        <?php
-        echo "</tr>";
-    }
-    echo ("</table>");
-
-    }else{
-        echo ('<h1 class="warnings">The Request complaint table is empty</h1>');
-    }
-////////////////////////////////////////////////////////////
-    //Sales table
-    $sql1="SELECT id,user_id,date,type,review,description FROM complaint";
-
-$result2=$con->query($sql1);
-
-//Shows the Request table details
-if ($result2->num_rows > 0) {
-
-    echo("<h1>Sale Complaints</h1>");
-    
-    echo("<table class='table1'>");
-
-    echo("<th>ID</th>");
-    echo("<th>User ID</th>");
-    echo("<th>Created date</th>");
-    echo("<th>Type</th>");
-    echo("<th>Review</th>");
-    echo("<th>Description</th>");
-    echo("<th>Action</th>");
-
-    while($row = $result2->fetch_assoc()) {
-        echo "<tr>";
-        echo "<td>".$row["id"]."</td>";
-        echo "<td>".$row["user_id"]."</td>";
-        echo "<td>".$row["date"]."</td>";
-        echo "<td>".$row["type"]."</td>";
-        echo "<td>".$row["review"]."</td>";
-        echo "<td>".$row["description"]."</td>";
-        ?>
-        <td><a class="delete1" href="php\controllers\complaint-ctrl.php?id=<?php echo $row["id"]; ?>">Delete</a></td>
-        <?php
+        echo "<td>".$row["reviewed"]."</td>";
+        echo "<th>delete</th>";
         echo "</tr>";
     }
     echo ("</table>");
@@ -125,8 +130,9 @@ if ($result2->num_rows > 0) {
         echo ('<h1 class="warnings">The Request complaint table is empty</h1>');
     }
 
-$con->close();
-        include("php/templates/footer.php");
+    $con->close();
+
+    include("php/templates/footer.php");
     ?>
 
 </body>
