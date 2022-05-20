673ce0f07a84e766ba716291e7b4f80ec8566568
it21294198 <99207728+it21294198@users.noreply.github.com>
2022-05-20 16:13:27 +0530

    compli

--- index.php
+++ index.php
@@ -38,7 +38,7 @@
     </div>
 <!--
 <div><a href="admin.php">Admin</a></div>
-<div><a href="complaint.php">complaint</a></div>
+<div><a href="complaints.php">complaint</a></div>
     -->
     <?php
         include("php/templates/footer.php");
