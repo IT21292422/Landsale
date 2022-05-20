efe49cedd0ce875b3d6163d9fc5d94e71454cbf2
it21294198 <99207728+it21294198@users.noreply.github.com>
2022-05-20 12:59:40 +0530

    correct3

--- about.php
+++ about.php
@@ -12,6 +12,18 @@
 
     <div class="mainpage">
         <h2>About Us</h2>
+        <p>Our approach gives our users a better
+experience when they intend to find or sell
+their land without having another
+third party person or company.</p>
+    </div>
+
+    <div class="mainpage">
+        <h2>Terms and Conditions</h2>
+        <p>Our approach gives our users a better
+experience when they intend to find or sell
+their land without having another
+third party person or company.</p>
     </div>
 
     <?php
