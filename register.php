691b7d125e1f965669937089013d2447a101db27
IT21294648 <c.wick400@gmail.com>
2022-05-20 21:21:31 +0530

    typo fix

--- register.php
+++ register.php
@@ -1,42 +1,82 @@
-<?php
-?>
+<?php require 'php/includes/dbcon.php';?>
+
 <!DOCTYPE html>
-<html lang="en">
-<head>
-    <title>Register</title>
+<html>
+	<head>
     <link rel="stylesheet" href="styles/headerfooter.css"/>
-</head>
-    <body>
+	</head>
+	
+	<body>
+		<!-- adding a header (c)-->
         <?php
             include("php/templates/header.php");
         ?>
+		<hr>
 
-        <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="post">
-            <fieldset>
+            <center><h1 id="cheader">Registration Form</h1></center>
 
-                <p>First Name: <input type="text" id="fname"  name="first_name" ></p>
-                <p>Last Name: <input type="text" id="lname" name="last_name" ></p>
-                <p>Email : <input type="email" id="email" name="email" ></p>
-                <p>Password : <input type="password" id="pswd" name="password"
-                    title="There must be one lowercase letter and one uppercase letter, and at leat 6 or more characters " > </p>
+            <form onsubmit="return checkPassword()" method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
 
-                <p>Confirm Password : <input type="password" name="confirm_password"></p><!--NEED JS-->
+                <label for="fname">First Name<br></label>
+				<input type="text" id="fname" name="fname" placeholder="First Name" required><br><br>
+				
+				<label for="lName">Last Name<br></label>
+				<input type="text" id="lName" name="lname" placeholder="Last Name" required><br><br>
 
-                <p>Phone : <input type="tel" id="phone" name="phone" required ></p><!--not sure about patterns-->
+                <label for="email">Email<br></label>
+				<input type="text" id="email" name="email" placeholder="abc@xyz.com" pattern="[a-z0-9._%+-]+@[a-z0-9]+\.[a-z]{2,3}" required><br><br>
+				
+                <label for="about">Add a small description about yourself<br></label>
+				<textarea id="aabout" rows="4" cols="25" name="about"></textarea><br><br>
 
-                <p>About me : <br><textarea type="text" name="about" id="aboutme" rows="7" cols="100" ></textarea></p>
+                <label for="photo">Upload a profile picture</label>
+                <input type="file" name="pPhoto" id="photo" ><br><br>
 
-                <p>Upload profile photo : <input type="file" name="profile_photo" id="prphoto" > </p> <!--Limit file size ? and only image style files ?-->
-                <p> <input type="checkbox" id="agreeT&C" name="agreeT&C" value="agreedT&C"> Agree to Terms and Conditions</p>
+                <label for="pass">Password</label><br>
+                <input type="password" name="pass" id="pass" pattern="(?=.+\d)(?=.+[a-z])(?=.+[A-Z]).{5,10}" required><br><br>
 
-            </fieldset>
-            <br>
-            <input type="submit" value="Submit" name="submit" >
-        </form>
+                <label for="repass">Re-Enter Password</label><br>
+                <input type="password" name="pass" id="repass"><br><br>
 
+                <label for="ToS">Accept Privacy Policy and Terms & Conditions</label><br>
+                <input type="checkbox" class="tos" name="tos" onclick="enableButton()"><br><br>
+
+                <center>
+                    <input type="submit" value="submit" id="submitbtn">
+                </center>
+            </form>
+        
+		<hr>
+		<!-- adding footer (g)-->
         <?php
             include("php/templates/footer.php");
-        ?>
-    </body>
-</html>
 
+            include("link.php");
+
+            //connecting to the DB
+            require 'php/includes/dbcon.php';
+            
+                $uID = $_POST[""];
+                $fName = $_POST["fname"];
+                $lName = $_POST["lname"];
+                $email = $_POST["email"];
+                $pNumber = $_POST["phone"];
+                $pass = $_POST["pass"];
+                $photo = $_POST["pPhoto"];
+            
+            
+                $query="INSERT INTO users(user_id, pass, first_name, last_name, email, account_status, account_type, 
+                profile_photo, about) VALUES (1, '$pass', '$fName', '$lName', '$email', 'Valid', 'User', '$photo', '$about' )";
+                
+                if($con->query($sql))
+                {
+                    echo "<script> alert ('Data added Successfully')</script>";
+                }
+                else
+                {
+                    echo "<script> alert ('Error: query was not Successful')</script>";
+                    echo sql;
+                }
+        ?>
+	</body>
+</html>
