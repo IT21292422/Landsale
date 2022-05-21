01f6eefcad288eeb811ac801a1e316f2932b39ab
ravii-sa <ravinduhapuarachchi@gmail.com>
2022-05-21 23:15:09 +0530

    delete-account styling

--- delete-account.php
+++ delete-account.php
@@ -14,9 +14,30 @@
     }
 
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Sale Post</title>
+    <link rel="stylesheet" href="styles/delete-account.css">
+    <?php include_once('php/includes/common-css-js.php'); ?>
 
-<form action="delete-account.php" method="post">
-    <h1>Delete your account?</h1>
-    <input type="button" value="Decline" onclick="location.href = 'index.php'">
-    <input type="submit" value="Accept">
-</form>
+</head>
+<body>
+    <?php
+        include("php/templates/header.php");
+    ?>
+    <form action="delete-account.php" method="post" class="delete-form">
+        <h1>Delete your account?</h1>
+        <div class="btns">
+            <input type="button" value="Decline" onclick="location.href = 'index.php'">
+            <input type="submit" value="Accept">
+        </div>
+
+    </form>
+
+    <?php
+        include("php/templates/footer.php");
+    ?>
+</body>
+</html>
+
