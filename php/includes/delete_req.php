6db6a76f873cfde27b7ec3d7c8ccb6a3857af9a8
Akmal <it21292422@my.sliit.lk>
2022-05-22 20:11:50 +0530

    fix

--- php/includes/delete_req.php
+++ php/includes/delete_req.php
@@ -4,7 +4,6 @@ $request_id=$_GET['id'];
 $sql="DELETE FROM request_phone WHERE request_id='$request_id';". "DELETE FROM saved_request WHERE request_id='$request_id';"."DELETE FROM request WHERE request_id='$request_id';";
 if($con->multi_query($sql)){
     echo "Deleted successfully<br>";
-    echo "<a href=\"../../owned-posts.php?id=$request_id\">Go Back</a>";
     
 }else{
     echo "Error: ".$con->error;
--- php/includes/delete_sale.php
+++ php/includes/delete_sale.php
@@ -1,15 +1,14 @@
 <?php
 require 'dbcon.php';
-$sale_id=$_GET['id'];
-$sql="DELETE FROM sale_phone WHERE sale_id='$sale_id';". 
-"DELETE FROM sale_complaints WHERE sale_id='$sale_id';".
-"DELETE FROM saved_sale WHERE sale_id='$sale_id';".
-"DELETE FROM sale_phone WHERE sale_id='$sale_id';".
-"DELETE FROM sale_media WHERE sale_id='$sale_id';";
-"DELETE FROM sale WHERE sale_id='$sale_id';";
+$id=$_GET['id'];
+$sql=  "DELETE FROM saved_sale WHERE sale_id = $id;".
+"DELETE FROM sale_phone WHERE sale_id = $id;".
+   "DELETE FROM sale_media WHERE sale_id = $id;".
+   "DELETE FROM sale_complaints WHERE sale_id = $id;".
+   "DELETE FROM sale WHERE sale_id = $id;";
 if($con->multi_query($sql)){
     echo "Deleted successfully<br>";
-    echo "<a href=\"../../owned-posts.php?id=$sale_id\">Go Back</a>";
+
 }else{
     echo "Error: ".$con->error;
 }
