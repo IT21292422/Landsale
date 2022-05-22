ab58fef9e605d55883d3b1ba9adb1b0826709681
Akmal <99236776+IT21292422@users.noreply.github.com>
2022-05-22 15:08:09 +0530

    Add files via upload

---
+++ php/includes/delete_req.php
@@ -0,0 +1,14 @@
+<?php
+require 'dbcon.php';
+$request_id=$_GET['id'];
+$sql="DELETE FROM request_phone WHERE request_id='$request_id'.
+        DELETE FROM request WHERE request_id='$request_id'";
+if($con->query($sql)){
+    echo "Deleted successfully<br>";
+    header("Location.owned-posts.php");
+}else{
+    echo "Error: ".$con->error;
+}
+
+
+?>
---
+++ php/includes/delete_sale.php
@@ -0,0 +1,14 @@
+<?php
+require 'dbcon.php';
+$sale_id=$_GET['id'];
+$sql="DELETE FROM sale_phone WHERE sale_id='$sale_id',
+        DELETE FROM sale WHERE sale_id='$sale_id'";
+if($con->query($sql)){
+    echo "Deleted successfully<br>";
+    header("Location.owned-posts.php");
+}else{
+    echo "Error: ".$con->error;
+}
+
+
+?>
