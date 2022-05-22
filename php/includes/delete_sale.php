c919075a179df4e963bfd561e08e11c273e83416
Akmal <99236776+IT21292422@users.noreply.github.com>
2022-05-22 16:42:07 +0530

    Update delete_sale.php

--- php/includes/delete_sale.php
+++ php/includes/delete_sale.php
@@ -1,14 +1,12 @@
 <?php
 require 'dbcon.php';
 $sale_id=$_GET['id'];
-$sql="DELETE FROM sale_phone WHERE sale_id='$sale_id',
-        DELETE FROM sale WHERE sale_id='$sale_id'";
-if($con->query($sql)){
+$sql="DELETE FROM sale_phone WHERE sale_id='$sale_id';". "DELETE FROM sale WHERE sale_id='$sale_id';";
+if($con->multi_query($sql)){
     echo "Deleted successfully<br>";
-    header("Location.owned-posts.php");
+    header("Location:owned-posts.php");
 }else{
     echo "Error: ".$con->error;
 }
-
 
 ?>
