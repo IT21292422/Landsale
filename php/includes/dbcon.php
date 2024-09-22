4c1d9486885a86646116991e334fb81a0dd9123b
IT21292422 <it21292422@my.sliit.lk>
2024-09-22 21:27:15 +0530

    fix: default configuration vulnerability and hard coded database credentials fixed

--- php/includes/dbcon.php
+++ php/includes/dbcon.php
@@ -1,13 +1,19 @@
 <?php
-    $server = "localhost";
-    $dbUname = "root";
-    $dbPassword = "";
-    $dbName = "landsale";
+    require_once 'vendor/autoload.php';
+    use Dotenv\Dotenv;
+
+    $dotenv = Dotenv::createImmutable(__DIR__ . '/../../');
+    $dotenv->load();
+
+    $server = $_ENV['DB_HOST'];
+    $dbUname = $_ENV['DB_USER'];
+    $dbPassword = $_ENV['DB_PASS'];
+    $dbName = $_ENV['DB_NAME'];
 
     $con = new mysqli($server, $dbUname, $dbPassword, $dbName);
 
     if($con->connect_error)
     {
-        die("connection to database failed:". $con->connect_error);
+        die("Connection to the database failed. Please try again later.");
     }
 ?>
