ba478c989ca4d2b09610aeaf7eafbed8dd61c6c6
chatwick <c.wick400@gmail.com>
2024-09-23 23:26:39 +0530

    fix: signin form input and url param sanitisation

--- signin.php
+++ signin.php
@@ -1,8 +1,4 @@
 <?php
-    //Name: H.A.R.S. Hapuarachchi
-    //IT Number: it21296246
-    //Center: Malabe
-    //Group: MLB_05.02_09
     include("php/controllers/signin-ctrl.php");
 ?>
 <!DOCTYPE html>
@@ -10,7 +6,7 @@
 <head>
     <title>Sign in</title>
     <link rel="stylesheet" href="styles/signin.css">
-    <link rel="stylesheet" href="styles/forms.css">    
+    <link rel="stylesheet" href="styles/forms.css">
     <?php include_once('php/includes/common-css-js.php'); ?>
 
 
@@ -21,22 +17,22 @@
    <form class="simple-form" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="post">
         <div class="form-field">
             <div for="email">E-mail</div>
-            <input type="email" name="email" value="<?php echo $values['email'] ?>">
-            <?php    if (!empty($errors['email'])) echo "<div class='error'>*".$errors['email']."</div><br>";    ?>
+            <input type="email" name="email" value="<?php echo htmlspecialchars($values['email']); ?>">
+            <?php if (!empty($errors['email'])) echo "<div class='error'>*" . htmlspecialchars($errors['email']) . "</div><br>"; ?>
         </div>
 
         <div class="form-field">
             <div for="pwd">Password</div>
-            <input type="password" name="password" value="<?php echo $values['password'] ?>">
-            <?php    if (!empty($errors['password'])) echo "<div class='error'>*".$errors['password']."</div><br>";    ?>
+            <input type="password" name="password" value="<?php echo htmlspecialchars($values['password']); ?>">
+            <?php if (!empty($errors['password'])) echo "<div class='error'>*" . htmlspecialchars($errors['password']) . "</div><br>"; ?>
         </div>
 
         <?php    if (!empty($errors['form'])) echo "<div class='form-field error'>*".$errors['form']."</div><br>";    ?>
 
-        <?php    if (!empty($_GET['redirect'])) echo "<input type='hidden' name='redirect' value='".$_GET['redirect']."'>";    ?>
+        <?php if (!empty($_GET['redirect'])) echo "<input type='hidden' name='redirect' value='" . htmlspecialchars($_GET['redirect']) . "'>"; ?>
 
         <div class="form-field">
-            <a href="register.php">Create an account</a>    
+            <a href="register.php">Create an account</a>
         </div>
 
 
@@ -44,7 +40,7 @@
         <input type="submit" name="submit" value="Submit">
         </div>
 
-   </form> 
+   </form>
    <?php include('php\templates\footer.php'); ?>
 
 </body>
