ea7e9a150fa9d6987412420a7266ce5e203c1b79
chamath-shanaka <chamathshanaka19@gmail.com>
2024-09-24 14:21:09 +0530

    [fix] SQL injection in reviewed

--- php/includes/complaintFunctions.php
+++ php/includes/complaintFunctions.php
@@ -6,67 +6,67 @@ Group: MLB_05.02_09-->
 
 <?php 
 
-function modAction($action, $uID)
-{
-    switch($action)
+    function modAction($action, $uID)
     {
-        case "warn":
-            warnUser($uID);
-            echo "<script>";
-            echo "alert (\"Successfully warned user\");";
-            echo "window.location.href = 'complaint.php';";
-            echo "</script>";     
-            break;
+        switch($action)
+        {
+            case "warn":
+                warnUser($uID);
+                echo "<script>";
+                echo "alert (\"Successfully warned user\");";
+                echo "window.location.href = 'complaint.php';";
+                echo "</script>";     
+                break;
 
-        case "suspend":
-            suspendUser($uID);
-            echo "<script>";
-            echo "alert (\"Successfully suspended user\");";
-            echo "window.location.href = 'complaint.php';";
-            echo "</script>";         
-            
-            break;
+            case "suspend":
+                suspendUser($uID);
+                echo "<script>";
+                echo "alert (\"Successfully suspended user\");";
+                echo "window.location.href = 'complaint.php';";
+                echo "</script>";         
+                
+                break;
 
-        case "ban":
-            banUser($uID);
-            echo "<script>";
-            echo "alert (\"Successfully banned user\");";
-            echo "window.location.href = 'complaint.php';";
-            echo "</script>";
-            break;
+            case "ban":
+                banUser($uID);
+                echo "<script>";
+                echo "alert (\"Successfully banned user\");";
+                echo "window.location.href = 'complaint.php';";
+                echo "</script>";
+                break;
 
-        case "noAction":
+            case "noAction":
+                echo "<script>";
+                echo "alert (\"No action taken\");";
+                echo "window.location.href = 'complaint.php';";
+                echo "</script>";
+                break;
+
+            default: 
             echo "<script>";
             echo "alert (\"No action taken\");";
             echo "window.location.href = 'complaint.php';";
             echo "</script>";
-            break;
-
-        default: 
-        echo "<script>";
-        echo "alert (\"No action taken\");";
-        echo "window.location.href = 'complaint.php';";
-        echo "</script>";
+        }
     }
-}
 
-function suspendUser($uID) // SQL injection fixed by using prepared statement
-{
-    global $con;
-    
-    $sql = "UPDATE users SET account_status='suspended' WHERE user_id=?";
-    $stmt = $con->prepare($sql);
-    
-    $stmt->bind_param('i', $uID);
-
-    if ($stmt->execute()) {
-        echo "<script> alert ('Successfully Suspended User')</script>";
-    } else {
-        echo "<script> alert ('Oops! Something went wrong')</script>";
-    }
-}
+    function suspendUser($uID) // SQL injection fixed by using prepared statement
+    {
+        global $con;
         
-function banUser($uID) // SQL injection fixed by using prepared statement
+        $sql = "UPDATE users SET account_status='suspended' WHERE user_id=?";
+        $stmt = $con->prepare($sql);
+        
+        $stmt->bind_param('i', $uID);
+
+        if ($stmt->execute()) {
+            echo "<script> alert ('Successfully Suspended User')</script>";
+        } else {
+            echo "<script> alert ('Oops! Something went wrong')</script>";
+        }
+    }
+            
+    function banUser($uID) // SQL injection fixed by using prepared statement
     {
         global $con;
 
@@ -82,22 +82,31 @@ function banUser($uID) // SQL injection fixed by using prepared statement
         }
     }
 
-function warnUser($uID) // SQL injection fixed by using prepared statement
+    function warnUser($uID) // SQL injection fixed by using prepared statement
+        {
+            global $con;
+
+            $sql = "UPDATE users_warnings SET warning='Warned' WHERE user_id=?";
+            $stmt = $con->prepare($sql);
+            
+            $stmt->bind_param('i', $uID);
+            
+            $stmt->execute();
+        }
+
+    function reviewed() // SQL injection fixed by using prepared statement
     {
         global $con;
 
-        $sql = "UPDATE users_warnings SET warning='Warned' WHERE user_id=?";
+        $sql = "UPDATE users SET account_status='banned' WHERE user_id=?";
         $stmt = $con->prepare($sql);
         
         $stmt->bind_param('i', $uID);
         
-        $stmt->execute();
+        if ($stmt->execute()) {
+            echo "<script> alert ('User account status updated to banned.')</script>";
+        } else {
+            echo "<script> alert ('Oops! Something went wrong')</script>";
+        }
     }
-
-function reviewed()
-{
-    global $con;
-
-    $sql = "UPDATE users SET account_status='banned' WHERE user_id=$uID";
-}
 ?>
