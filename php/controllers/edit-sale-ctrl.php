f69681cf1f89954e4949e87a266cd68c0b1c6f21
chamath-shanaka <chamathshanaka19@gmail.com>
2024-09-25 21:04:53 +0530

    [fix] SQL injection in edit-sale-ctrl

--- php/controllers/edit-sale-ctrl.php
+++ php/controllers/edit-sale-ctrl.php
@@ -8,33 +8,38 @@ require_once('../includes/dbcon.php');
 //id must be valid and not null 
 if(isset($_GET['id']) ){
 
-    $id=$_GET['id'];
-    $stitle =$_POST['stitle'];
-    $sloc =$_POST["slocation"];
-    $sdesc =$_POST["sdescript"];
-    $scity =$_POST["scity"];
-    $sdist =$_POST["sdistrict"];
-    $sprovince =$_POST["sprovince"];
-    $sprice =$_POST["sprice"];
-    $sarea =$_POST["slandarea"];
-    $saddress =$_POST["saddress"];
-    $sphoto =$_POST["sphoto"];
+    $id = $_GET['id'];
+    $stitle = $_POST['stitle'];
+    $sloc = $_POST["slocation"];
+    $sdesc = $_POST["sdescript"];
+    $scity = $_POST["scity"];
+    $sdist = $_POST["sdistrict"];
+    $sprovince = $_POST["sprovince"];
+    $sprice = $_POST["sprice"];
+    $sarea = $_POST["slandarea"];
+    $saddress = $_POST["saddress"];
+    $sphoto = $_POST["sphoto"];
 
-    $sql= "UPDATE `sale` SET `title`='$stitle',`location`='$sloc',`description`='$sdesc',
-    `city`='$scity',`district`='$sdist',`province`='$sprovince',`price`='$sprice',`land_area`='$sarea',
-    `address`='$saddress',`cover_photo`='$sphoto' WHERE sale_id=$id";
+    // SQL injection fixed by using prepared statement
+    $sql = "UPDATE `sale` SET `title`=?, `location`=?, `description`=?, 
+    `city`=?, `district`=?, `province`=?, `price`=?, `land_area`=?, 
+    `address`=?, `cover_photo`=? WHERE sale_id=?";
 
-if($con->query($sql)==TRUE  ){
-    echo "<script> alert ('Updated Successfully')</script>";
-    
+    if ($stmt = $con->prepare($sql)) {
+        // SQL injection fixed by using prepared statement
+        $stmt->bind_param("ssssssssssi", $stitle, $sloc, $sdesc, $scity, $sdist, $sprovince, $sprice, $sarea, $saddress, $sphoto, $id);
+
+        if ($stmt->execute()) {
+            echo "<script> alert('Updated Successfully')</script>";
+        } else {
+            echo "<script> alert('Error: query was not Successful')</script>";
+        }
+        $stmt->close();
+
+    } else {
+        echo "<script> alert('Error: could not prepare query')</script>";
+    }
+} else {
+    echo "Invalid";
 }
-else{
-    echo "<script> alert ('Error: query was not Successful')</script>";
-    echo sql;
-}
-
-}else{
-    echo"invald";
-}
-
 ?>
