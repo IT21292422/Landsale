daf8be6eb0d1bce9a16c183ae1e42ed55f994987
chamath-shanaka <chamathshanaka19@gmail.com>
2024-09-25 21:13:51 +0530

    [fix] SQL injection & application error disclosure

--- php/controllers/request-complaints-ctrl.php
+++ php/controllers/request-complaints-ctrl.php
@@ -7,19 +7,25 @@
 require '../includes/dbcon.php';
 
 echo $_GET["complaint_id"];
+$sql1 = "DELETE FROM request_complaints WHERE complaint_id = ?";
 
-$sql1 = "DELETE FROM request_complaints WHERE complaint_id='". $_GET["complaint_id"]."'";
-
-if ($con->query($sql1)) {
+// SQL injection fixed by using prepared statement
+if ($stmt = $con->prepare($sql1)) {
+    $stmt->bind_param("i", $_GET["complaint_id"]);
     
-    if (isset($_SERVER["HTTP_REFERER"])){
-        header("Location: " . $_SERVER["HTTP_REFERER"]);
-    }
+    if ($stmt->execute()) {
+        
+        if (isset($_SERVER["HTTP_REFERER"])) {
+            header("Location: " . $_SERVER["HTTP_REFERER"]);
+        }
 
+    } else {
+        echo '<script type="text/javascript">alert("Record was not deleted!!!");</script>';
+        echo "Error deleting record: "; // fixed application error disclosure ( . mysqli_error($con))
+    }
 } else {
     
-    echo '<script type="text/javascript">alert("Recode was not deleted!!!");</script>';
-    echo "Error deleting record: " . mysqli_error($con);
+    echo '<script type="text/javascript">alert("Error preparing the query!!!");</script>';
 }
 
 ?>
